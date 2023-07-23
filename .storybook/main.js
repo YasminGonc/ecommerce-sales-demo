dbed944110d832c767b3b5bc6404ec31a288f2f2
YasminGonc <yasmin.goncalves97@hotmail.com>
2023-07-23 15:31:18 -0300

    Update packages

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,8 @@
 module.exports = {
-  "stories": [
-    "../src/components/**/*.stories.mdx",
-    "../src/components/**/stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
+  "stories": ["../src/components/**/*.stories.mdx", "../src/components/**/stories.@(js|jsx|ts|tsx)"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
+  "framework": {
+    name: "@storybook/nextjs",
+    options: {}
   }
-}
+};
